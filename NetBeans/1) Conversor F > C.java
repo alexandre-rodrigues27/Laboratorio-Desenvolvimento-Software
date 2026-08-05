c6317b291064e461bc8a3b04bc1c2580939118e9
alexandre-rodrigues27 <rodriguesxandy82@gmail.com>
2026-08-05 11:43:27 -0300

    Update and rename 1) Conversor F > C to 1) Conversor F > C.java

--- NetBeans/1) Conversor F > C.java
+++ NetBeans/1) Conversor F > C.java
@@ -1,3 +1,5 @@
+//Código digitado a partir do botão adicionado
+
 private void btnConverterActionPerformed(java.awt.event.ActionEvent evt) {                                             
         double fahrenheit = Double.parseDouble(txtTemp.getText());
         double celsius = (fahrenheit - 32.0) / 1.8;
